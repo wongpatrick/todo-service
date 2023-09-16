3c98b23835828d8967f4bcd66b12ac0759c4e264
Patrick Wong <wp.patrickwong@gmail.com>
2023-09-16 01:30:23 -0400

    Initialize repo with a basic SQL script and some vendors

---
+++ sql-scripts/create_database.sql
@@ -0,0 +1,17 @@
+CREATE SCHEMA todoapp ;
+
+CREATE TABLE todoapp.user (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+);
+
+
+CREATE TABLE todoapp.task (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES todoapp.user(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  status VARCHAR(50) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+);
