2f9a9d055f5d4eb59ed974b3cdd5f71848d24f96
Patrick Wong <wp.patrickwong@gmail.com>
2023-09-16 03:37:26 -0400

    Added comments for fetch task for user

--- api/services/fetchtaskforuser.go
+++ api/services/fetchtaskforuser.go
@@ -8,6 +8,7 @@ import (
 // Fetch Listing with params
 func FetchTaskForUser(taskParams model.TaskParams) ([]model.Task, error) {
 	// TODO: Authenticate User
+	// Make sure user has access to only their todo list
 
 	taskData, queryErr := repository.FetchTask(taskParams)
 	if queryErr != nil {
